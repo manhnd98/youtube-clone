e5b4354e202bf99d6f093a0aee191c29461204b6
Mạnh Nguyễn <manhnd.jin@gmail.com>
2022-11-11 16:52:48 +0700

    chore: add prisma migration query and lint

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+const base = require('@tinkoff/prettier-config/angular');
+
+module.exports = {
+  ...base,
+  overrides: [
+    ...base.overrides,
+    {
+      files: ['*.js', '*.ts'],
+      options: {printWidth: 90, parser: 'typescript'},
+    },
+    {
+      files: '*.html',
+      options: {printWidth: 80, parser: 'html'},
+    },
+    {
+      files: ['package.json', 'ng-package.json'],
+      options: {parser: 'json-stringify'},
+    }
+    ],
+};
